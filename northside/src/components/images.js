b9b89111428aa02fea0289e347ba6f99ac3e22ff
Dani Barisic <barisicdani@gmail.com>
2025-04-14 17:12:39 +0200

    Deleted old .js files which were replaced with .jsx versions

--- northside/src/components/images.js
+++
@@ -1,9 +0,0 @@
-import logo from "../assets/images/soccer-ball.png"
-import ballLogo from "../assets/images/nysl_logo.png"
-
-const images = {
-    logo: logo,
-    ballLogo: ballLogo
-};
-
-export default images;
