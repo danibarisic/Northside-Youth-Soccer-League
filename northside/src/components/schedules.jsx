c7c6f04c89884b0160843d2e111881aee7dce1d5
Dani Barisic <barisicdani@gmail.com>
2025-04-15 14:44:31 +0200

    Added second locations table

--- northside/src/components/schedules.jsx
+++ northside/src/components/schedules.jsx
@@ -1,30 +1,48 @@
 import React, { } from 'react';
 import "../index.css"
 import { GameTable } from './gametable';
+import { LocationTable } from './locationlist';
 
 export const Schedules = () => {
     return (
         <>
             <div className="container-schedule">
-                <div className="container-content">
+                <div className="container-content py-3">
                     <h2>Game Schedules</h2>
                     <h3>Winter Season</h3>
                 </div>
-                <div className="table-container">
-                    <table className="table-schedule">
-                        <thead className="header-row">
+                <div>
+                    <table className="table table-striped table-dark table-hover">
+                        <thead className="thead-dark">
                             <tr>
-                                <th>Date</th>
-                                <th>Time</th>
-                                <th>Team 1</th>
-                                <th>Team 2</th>
-                                <th>Location</th>
+                                <th scope="col">Date</th>
+                                <th scope="col">Time</th>
+                                <th scope="col">Team 1</th>
+                                <th scope="col">Team 2</th>
+                                <th scope="col">Location</th>
                             </tr>
                         </thead>
                         <tbody>
                             <GameTable />
                         </tbody>
                     </table>
+                    <h3>Game Locations</h3>
+                    <table className="table table-striped table-dark table-hover">
+                        <thead className="thead-dark">
+                            <tr>
+                                <th scope="col">Name</th>
+                                <th scope="col">Directions</th>
+                                {/* <th scope="col">Map URL</th> */}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <LocationTable />
+                        </tbody>
+                    </table>
+
+                </div>
+                <div>
+
                 </div>
             </div>
         </>
