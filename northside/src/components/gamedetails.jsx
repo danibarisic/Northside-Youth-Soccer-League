1f9bf4d95e05bde2e6779c3c8cd6fb5fbae861f6
Dani Barisic <barisicdani@gmail.com>
2025-04-16 09:07:11 +0200

    Added Match Details Component

--- northside/src/components/gamedetails.jsx
+++ northside/src/components/gamedetails.jsx
@@ -1,15 +1,36 @@
-import React, { useState, useEffect } from 'react';
+import React, { } from 'react';
 import "../index.css"
-import { useNavigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import games from "../database.json";
 
 export const GameDetails = () => {
-    const navigate = useNavigate();
-    const gameData = games;
-    const listOfGames = gameData[1];
-    console.log(listOfGames);
+    const { id } = useParams();
+    const arrayOfGames = Object.values(games);
+    const listOfGames = arrayOfGames[1];
+    const gameList = Object.values(listOfGames);
+    const game = gameList.find((g) => String(g.id) === id);
+
+    const locations = arrayOfGames[0];
+    const matchedLocation = locations[game.location];
+
+    if (!game) return <p>Game not found...</p>
 
     return (
-        listOfGames
+        <>
+            <div className="container-details">
+                <h2>Match Details</h2>
+                <h2>{game.team1} vs {game.team2}</h2>
+                <h3>{game.date}</h3>
+                <h3>{game.time}</h3>
+                <h3>{game.location}</h3>
+            </div>
+            <div className="map-details">
+                {matchedLocation?.iframe && (
+                    <iframe title={locations.name}
+                        src={matchedLocation.iframe}
+                    />
+                )}
+            </div>
+        </>
     )
 }
